d19533d456f5ef0a46efc0881afef3c3113aa59a
Emerson Thompson <thompson.emerson@gmail.com>
2015-06-30 11:44:26 -0300

    Integração com Firebase

--- app/controllers.js
+++ app/controllers.js
@@ -1,15 +1,38 @@
 // Incluindo o modulo
 angular.module('simpleApp')
 
-// Controllers
+
+// Controller Home
 .controller('HomeCtrl', ['$scope', function ($scope) {
-	$scope.titulo = "Página Inicial";
+	$scope.titulo = "Welcome ;D";
 }])
 
-.controller('SobreCtrl', ['$scope', function ($scope) {
-	$scope.titulo = "Sobre nós";
-}])
 
-.controller('ContatoCtrl', ['$scope', function ($scope) {
-	$scope.titulo = "Fale Conosco";
+// Controller Firebase
+.controller('FirebaseCtrl', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+	$scope.titulo = "Firebase";
+
+	// Conectando com Firebase na Collection 'Equipes'
+	var ref = new Firebase("https://single-app.firebaseio.com/equipes");
+
+	// Recebendo dados
+	$scope.equipes = $firebaseArray(ref);
+
+	// Adicionando nova Equipe
+	$scope.addEquipe = function() {
+		$scope.equipes.$add({
+			nome: $scope.nome,
+			thumb: $scope.thumb
+		});
+		$scope.nome = "";
+		$scope.thumb = "";
+		Materialize.toast('Equipe Adicionada', 3000);
+	};
+
+	// Removendo Equipe
+	$scope.removerEquipe = function(equipe) {
+		$scope.equipes.$remove(equipe);
+		Materialize.toast('Equipe Removida', 3000);
+	};
+
 }]);
